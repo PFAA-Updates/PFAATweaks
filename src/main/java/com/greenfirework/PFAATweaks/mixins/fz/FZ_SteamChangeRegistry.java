12687179a960ba0ad4fc44182ba4c2a8e66c4b9f
Sukasa <nick@greenfirework.com>
2024-08-06 06:49:16 -0700

    Fix two compile errors

--- src/main/java/com/greenfirework/PFAATweaks/mixins/fz/FZ_SteamChangeRegistry.java
+++ src/main/java/com/greenfirework/PFAATweaks/mixins/fz/FZ_SteamChangeRegistry.java
@@ -3,13 +3,11 @@ package com.greenfirework.PFAATweaks.mixins.fz;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Constant;
-import factorization.charge.TileEntitySteamTurbine;
 import factorization.common.Registry;
-import factorization.shared.TileEntityCommon;
 
 
-@Mixin(value = Registry.class, remap = true)
-public abstract class FZ_SteamChangeRegistry extends TileEntityCommon {
+@Mixin(value = Registry.class, remap = false)
+public abstract class FZ_SteamChangeRegistry {
 
 	@ModifyConstant(method = "makeItems()V", require = 1, constant = @Constant(stringValue = "steam"))
 	public String constantValue(String oldValue) {
--- src/main/java/com/greenfirework/PFAATweaks/mixins/fz/FZ_SteamChangeSolarBoiler.java
+++ src/main/java/com/greenfirework/PFAATweaks/mixins/fz/FZ_SteamChangeSolarBoiler.java
@@ -7,7 +7,7 @@ import factorization.charge.TileEntitySolarBoiler;
 import factorization.shared.TileEntityCommon;
 
 
-@Mixin(value = TileEntitySolarBoiler.class, remap = true)
+@Mixin(value = TileEntitySolarBoiler.class, remap = false)
 public abstract class FZ_SteamChangeSolarBoiler extends TileEntityCommon {
 
 	@ModifyConstant(method = {"setupSteam()V", "putData(Lfactorization/api/datahelpers/DataHelper;)V"}, require = 1, constant = @Constant(stringValue = "steam"))
