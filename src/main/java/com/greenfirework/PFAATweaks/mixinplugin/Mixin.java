cb701cee83a97fba394fb1495b980f25d82ade72
Chris Stone <chris.stone@cjsystemsonline.com>
2025-01-05 23:43:12 -0700

    Disable MatterOverdrive Mixins since there implemented in source now.

--- src/main/java/com/greenfirework/PFAATweaks/mixinplugin/Mixin.java
+++ src/main/java/com/greenfirework/PFAATweaks/mixinplugin/Mixin.java
@@ -14,7 +14,7 @@ public enum Mixin {
     IE_SILO_TANK_SIZE("ie.IE_SheetmetalTankCapacityBuff", IMMERSIVE_ENG),
     IE_WINDMILL_NERF("ie.IE_WindmillBalancing", IMMERSIVE_ENG),
     IE_WATERMILL_NERF("ie.IE_WatermillBalancing", IMMERSIVE_ENG),
-    
+
     // IndustrialCraft 2 tweaks
     IC2_MINER_SPEED_FIX("ic2.IC2_MinerSpeedFix", INDUSTRIALCRAFT),
     IC2_NANOSABER_FIX("ic2.IC2_NanosaberDamageFix", INDUSTRIALCRAFT),
@@ -22,35 +22,35 @@ public enum Mixin {
     IC2_UUTWEAK("ic2.IC2_UUMatterFix", INDUSTRIALCRAFT),
     IC2_HXFIX("ic2.IC2_LiquidHXFluidFix", INDUSTRIALCRAFT),
     IC2_STEAMGENFIX("ic2.IC2_SteamGeneratorFix", INDUSTRIALCRAFT),
-    
+
     // Advanced Lightsabers
     AL_ORE_GEN_FIX("al.AL_OreDimensionSwitch", LIGHTSABERS),
     AL_STRUCTURE_GEN_FIX("al.AL_StructureDimensionChanges", LIGHTSABERS),
     AL_SABER_DAMAGE_FIX("al.AL_SaberDamageFix", LIGHTSABERS),
     AL_STRUCTURE_SPREAD_FIX("al.AL_StructurePositionChange", LIGHTSABERS),
-    
+
     // Matter Overdrive
-    MO_PHASER_DAMAGE_FIX("mo.MO_PhaserDamageFix", MATTER_OVERDRIVE),
-    MO_ION_SNIPER_DAMAGE_FIX("mo.MO_IonSniperDamageFix", MATTER_OVERDRIVE),
-    MO_PLASMA_SHOTGUN_DAMAGE_FIX("mo.MO_PlasmaShotgunDamageFix", MATTER_OVERDRIVE),
-    MO_OMNI_TOOL_DAMAGE_FIX("mo.MO_OmniToolDamageFix", MATTER_OVERDRIVE),
-    MO_ANDROID_HP_FIX("mo.MO_RogueAndroidMobBuffs", MATTER_OVERDRIVE),
-    
+    //MO_PHASER_DAMAGE_FIX("mo.MO_PhaserDamageFix", MATTER_OVERDRIVE),
+    //MO_ION_SNIPER_DAMAGE_FIX("mo.MO_IonSniperDamageFix", MATTER_OVERDRIVE),
+    //MO_PLASMA_SHOTGUN_DAMAGE_FIX("mo.MO_PlasmaShotgunDamageFix", MATTER_OVERDRIVE),
+    //MO_OMNI_TOOL_DAMAGE_FIX("mo.MO_OmniToolDamageFix", MATTER_OVERDRIVE),
+    //MO_ANDROID_HP_FIX("mo.MO_RogueAndroidMobBuffs", MATTER_OVERDRIVE),
+
     // MagnetiCraft
-    //MG_FUEL_DISABLE("mg.MG_DisableFuelManager", MAGNETICRAFT),    
-    //MG_CRUSHER_TWEAK("mg.MG_TweakCrusherPower", MAGNETICRAFT),    
-    //MG_GRINDER_TWEAK("mg.MG_TweakGrinderPower", MAGNETICRAFT),    
-    //MG_ENERGY_VALUES("mg.MG_EnergyConverterPatches", MAGNETICRAFT),    
-    
+    //MG_FUEL_DISABLE("mg.MG_DisableFuelManager", MAGNETICRAFT),
+    //MG_CRUSHER_TWEAK("mg.MG_TweakCrusherPower", MAGNETICRAFT),
+    //MG_GRINDER_TWEAK("mg.MG_TweakGrinderPower", MAGNETICRAFT),
+    //MG_ENERGY_VALUES("mg.MG_EnergyConverterPatches", MAGNETICRAFT),
+
     // Techguns
     TG_DAMAGE_SCALING("tg.TG_DamageRescale", TECHGUNS),
-    
+
     // Factorization
     FZ_STEAM_REGISTRY("fz.FZ_SteamChangeRegistry", FACTORIZATION),
     FZ_STEAM_SHAFT("fz.FZ_SteamChangeShaft", FACTORIZATION),
     FZ_STEAM_BOILERT("fz.FZ_SteamChangeSolarBoiler", FACTORIZATION),
     FZ_STEAM_TURBINE("fz.FZ_SteamChangeTurbine", FACTORIZATION),
-    
+
     // Railcraft
     RC_ACCESSOR("rc.RC_TurbineInventoryAccessor", RAILCRAFT),
     RC_TURBINE_INVENTORY("rc.RC_TurbineInventoryAccess", RAILCRAFT),
@@ -59,7 +59,7 @@ public enum Mixin {
     RC_LOCO_NOCRAFT("rc.RC_NoLocoCrafting", RAILCRAFT),
     RC_COLOR_SLOT("rc.RC_SlotColorFilterFix", RAILCRAFT);
 
-	
+
 
 
     public final String mixinClass;
