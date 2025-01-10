f46a4fee4c2d674cc55f2acd7bc3900f16c96d84
Sukasa <nick@greenfirework.com>
2025-01-09 22:26:52 -0800

    Rework mixin to try and correct incorrect getPower()D modification

--- src/main/java/com/greenfirework/PFAATweaks/mixins/ie/IE_WatermillBalancing.java
+++ src/main/java/com/greenfirework/PFAATweaks/mixins/ie/IE_WatermillBalancing.java
@@ -1,8 +1,9 @@
 package com.greenfirework.PFAATweaks.mixins.ie;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import blusunrize.immersiveengineering.common.blocks.TileEntityIEBase;
 import blusunrize.immersiveengineering.common.blocks.wooden.TileEntityWatermill;
@@ -10,8 +11,8 @@ import blusunrize.immersiveengineering.common.blocks.wooden.TileEntityWatermill;
 @Mixin(value = TileEntityWatermill.class, remap = false)
 public abstract class IE_WatermillBalancing extends TileEntityIEBase {
 	
-	@ModifyArg(method = "getPower()D", at=@At("RETURN"))
-	public double scalePower(double retVal) {
-		return retVal * 0.2;
+	@Inject(method = "getPower()D", at=@At("RETURN"), cancellable = true)
+	public void scalePower(CallbackInfoReturnable<Double> cir) {
+		cir.setReturnValue(cir.getReturnValue() * 0.2);
 	}
 }
