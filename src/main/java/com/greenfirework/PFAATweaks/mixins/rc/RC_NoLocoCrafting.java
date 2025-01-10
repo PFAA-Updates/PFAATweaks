e8050ce417d721365a6dbfa1dc24a23551b5b6a4
Sukasa <nick@greenfirework.com>
2025-01-09 23:00:40 -0800

    Correct mixin to use static method redirect signature

--- src/main/java/com/greenfirework/PFAATweaks/mixins/rc/RC_NoLocoCrafting.java
+++ src/main/java/com/greenfirework/PFAATweaks/mixins/rc/RC_NoLocoCrafting.java
@@ -5,13 +5,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.At;
 
 import mods.railcraft.common.modules.ModuleLocomotives;
-import mods.railcraft.common.plugins.forge.CraftingPlugin;
 import net.minecraft.item.ItemStack;
 
 @Mixin(value = ModuleLocomotives.class, remap = false)
 public class RC_NoLocoCrafting {
 	@Redirect(method = "initFirst()V", at = @At(value = "INVOKE", target = "Lmods/railcraft/common/plugins/forge/CraftingPlugin;addShapedRecipe(Lnet/minecraft/item/ItemStack;[Ljava/lang/Object;)V"))
-	public void NoRecipe(CraftingPlugin instance, ItemStack result, Object... recipeArray) {
+	public void NoRecipe(ItemStack result, Object... recipeArray) {
 		// Do nothing - disable the shaped locomotive recipes so we can add them via CraftTweaker
 	}
 }
